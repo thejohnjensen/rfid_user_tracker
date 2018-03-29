e6906553daa30556611c21d09f92d9c0d36e1ce3
jensjoh01 <johnnyj2439@gmail.com>
2018-03-29 09:48:41 -0700

    Student model created

---
+++ rfid_app/models.py
@@ -0,0 +1,18 @@
+"""Create database models."""
+from app import db
+from datetime import datetime
+
+
+class Student(db.Model):
+    """Create model for students."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text())
+    last_name = db.Column(db.Text())
+    home_stop = db.Column(db.Text())
+    school_stop = db.Column(db.Text())
+    present = db.Column(db.Boolean(), default=False)
+
+    def __repr__(self):
+        """Name of object."""
+        return '<Student {} {}>'.format(self.first_name, self.last_name)

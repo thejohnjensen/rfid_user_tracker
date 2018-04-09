c12deb842855d1c9d10dcca1d1e46d9ae295a350
jensjoh01 <johnnyj2439@gmail.com>
2018-04-09 11:11:53 -0700

    able to get bus stops for bus number and render

--- rfid_app/routes.py
+++ rfid_app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from rfid_app import app, db, Config
 import pandas as pd
 from rfid_app.models import Bus, BusStop
@@ -11,12 +11,11 @@ conn_str = Config.SQLALCHEMY_DATABASE_URI
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # import pdb; pdb.set_trace()
-    print(request.path)
-    if request.method == 'POST':
-        pass
 
-    form = BusNumber()
+    if request.method == 'POST':
+        bus_number = request.form['bus_number']
+        return redirect(url_for('bus_stops', number=bus_number))
+
     data = pd.read_sql('SELECT students.first_name, students.last_name, students.present, \
                 bus_stops.name AS bus_stop, routes.name AS route_name, buses.number AS bus_number\
                 FROM bus_stops, bus_stop_route, students, routes, buses, bus_route \
@@ -25,13 +24,32 @@ def index():
                 AND bus_stop_route.route_id=routes.id \
                 AND bus_route.bus_id=buses.id \
                 AND bus_stop_route.route_id=bus_route.route_id', con=conn_str)
+
     buses = Bus.query.all()
-    stops = BusStop.query.all()
-    # import pdb; pdb.set_trace()
-    return render_template('index.html', data=data.to_html(), buses=buses, stops=stops, form=form)
+
+    return render_template('index.html', data=data.to_html(), buses=buses)
 
 
-@app.route('/bus/', methods=['GET', 'POST'])
-def bus_stops():
+@app.route('/bus/<number>', methods=['GET', 'POST'])
+def bus_stops(number):
     # import pdb; pdb.set_trace()
-    return "Hello"
+    data = pd.read_sql('SELECT students.first_name, students.last_name, students.present, \
+                bus_stops.name AS bus_stop, routes.name AS route_name, buses.number AS bus_number\
+                FROM bus_stops, bus_stop_route, students, routes, buses, bus_route \
+                WHERE students.home_stop_id=bus_stop_route.stop_id \
+                AND students.home_stop_id=bus_stops.id \
+                AND bus_stop_route.route_id=routes.id \
+                AND bus_route.bus_id=buses.id \
+                AND bus_stop_route.route_id=bus_route.route_id \
+                AND buses.number={}'.format(number), con=conn_str)
+
+    bus_stops = pd.read_sql('SELECT bus_stops.name AS bus_stop \
+            FROM bus_stops, buses, bus_stop_route, bus_route \
+            WHERE buses.number={} \
+            AND bus_stops.id=bus_stop_route.stop_id \
+            AND bus_stop_route.route_id=bus_route.route_id \
+            AND bus_route.bus_id=buses.id'.format(number), con=conn_str)
+    # bus_stops_iter = bus_stops.itertuples()
+    bus = Bus.query.filter_by(number=number).first()
+    # import pdb; pdb.set_trace()
+    return render_template('bus_detail.html', data=data.to_html(), bus=bus)
